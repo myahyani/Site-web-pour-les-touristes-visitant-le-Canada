12edcba5dba6129bd070898406b4f40c11ab10c4
myahyani <yahyanimohamed92@gmail.com>
2020-10-28 20:53:18 -0400

    last 28/10 version

--- F_plainte.php
+++ F_plainte.php
@@ -4,12 +4,11 @@ $text= "\nNom:".$_POST["Nom"]." Prénom:".$_POST["Prenom"]." E-mail:".$_POST["em
 file_put_contents('plaintes.txt', $text, FILE_APPEND);
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <link rel="icon" href="img/icon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="encoding">
     <title>YahyaniTours</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
@@ -39,48 +38,64 @@ file_put_contents('plaintes.txt', $text, FILE_APPEND);
     <div style="height: 65%;"></div>
     
     
-<!--  FOOTER START -->
-  
-<div class="footer">
-    <div class="inner-footer">
-        <div class="footer-items">
-            <h1 style="margin-bottom: 5px;">Yahyani<span class="text-primary">Tours<i class="ri-compass-discover-line"></i></span></h1>
-            <p>WALKING TOURS IN FIVE<br>CANADIAN CITIES.</p>
-        </div>
-        <div class="footer-items">
-            <h3>Quick Links</h3>
-            <ul>
-                <a href="index.html"><li>Accueil</li></a>
-                <a href="plainte.php"><li>Plainte</li></a>
-                <a href="apropos.html"><li>About</li></a>
-            </ul>
-        </div>
-        <div class="footer-items">
-            <h3>Cities</h3>
-            <ul>
-                <a href="Cities.php?qst=Toronto"><li>Toronto</li></a>
-                <a href="Cities.php?qst=Vancouver"><li>Vancouver</li></a>
-                <a href="Cities.php?qst=Hamilton"><li>Hamilton</li></a>
-                <a href="Cities.php?qst=Montreal"><li>Montreal</li></a>
-                <a href="Cities.php?qst=Ottawa"><li>Ottawa</li></a>
-            </ul>
-        </div>
-        <div class="footer-items">
-            <h3>Contact us</h3>
-            <ul>
-                <li><i class="fa fa-map-marker"></i>Montreal Gouin Boulevard, Canada</li>
-                <li><i class="fa fa-envelope"></i>yahyaniTours@gmail.com</li>
-                <li><i class="fa fa-phone"></i>+1 (514) 863-2918</li>
-            </ul> 
-            <div class="social-media">
-                <a href="#"><i class="ri-instagram-fill"></i></a>
-                <a href="#"><i class="ri-facebook-circle-fill"></i></a>
-                <a href="#"><i class="ri-youtube-fill"></i></a>
-            </div> 
+    <!--  FOOTER START -->
+
+    <div class="footer">
+        <div class="inner-footer">
+            <div class="footer-items">
+                <h1 style="margin-bottom: 5px;">Yahyani<span class="text-primary">Tours<i class="ri-compass-discover-line"></i></span></h1>
+                <p>WALKING TOURS IN FIVE<br>CANADIAN CITIES.</p>
+            </div>
+            <div class="footer-items">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="index.html">
+                        Accueil</a></li>
+
+                    <li><a href="plainte.php">
+                        Plainte</a></li>
+
+                    <li><a href="apropos.html">
+                        About</a></li>
+
+                </ul>
+            </div>
+            <div class="footer-items">
+                <h3>Cities</h3>
+                <ul>
+                    <li><a href="Cities.php?qst=Toronto">
+                        Toronto</a></li>
+
+                    <li><a href="Cities.php?qst=Vancouver">
+                        Vancouver</a></li>
+
+                    <li><a href="Cities.php?qst=Hamilton">
+                        Hamilton</a></li>
+
+                    <li><a href="Cities.php?qst=Montreal">
+                        Montreal</a></li>
+
+                    <li><a href="Cities.php?qst=Ottawa">
+                        Ottawa</a></li>
+
+                </ul>
+            </div>
+            <div class="footer-items">
+                <h3>Contact us</h3>
+                <ul>
+                    <li><i class="fa fa-map-marker"></i>Montreal Gouin Boulevard, Canada</li>
+                    <li><i class="fa fa-envelope"></i>yahyaniTours@gmail.com</li>
+                    <li><i class="fa fa-phone"></i>+1 (514) 863-2918</li>
+                </ul>
+                <div class="social-media">
+                    <a href="#"><i class="ri-instagram-fill"></i></a>
+                    <a href="#"><i class="ri-facebook-circle-fill"></i></a>
+                    <a href="#"><i class="ri-youtube-fill"></i></a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 
-<script src="script.js"></script>
 </body>
+
 </html>
